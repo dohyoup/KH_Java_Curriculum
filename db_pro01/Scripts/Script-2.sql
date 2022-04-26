8bfa12e191e2601026321c8121de2638d1c2630f
dohyoup <dohyoup@naver.com>
2022-04-26 16:11:08 +0900

    SQL함수, 예제풀이

---
+++ db_pro01/Scripts/Script-2.sql
@@ -0,0 +1,169 @@
+SELECT FIRST_NAME
+	 , LENGTH(FIRST_NAME)
+  FROM EMPLOYEES;
+  
+SELECT LENGTH('Hello') AS Col1
+	 , LENGTH('안녕') AS Col2
+	 , LENGTHB('Hello') AS Col3
+	 , LENGTHB('안녕') AS Col4
+	FROM DUAL; --더미테이블이 저장될 수 있는 거 = 임시 테이블정도
+	
+	
+SELECT INSTR('sample@example.com', '@') AS Col1 
+-- 인덱스 에스티알 = 공백의 위치가 어디에 위치해 있는가 DB 위치셀때는 1부터 센다. /자바는 0부터 차이
+	 , INSTR('sample@example.com', '@', -1) AS Col2
+	  -- -1 해주면 오른쪽에서 왼쪽으로 숫자를 세고 안쓰면 자동으로 1로 인식하고 왼쪽에서 오른쪽으로 찾는다.
+	 , INSTR('sample@example.com', 'e') AS Col3
+	 , INSTR('sample@example.com', 'e', -1) AS Col4
+	 , INSTR('sample@example.com', 'e', 1, 2) AS Col5
+	 , INSTR('sample@example.com', 'e', -1, 2) AS Col6
+  FROM DUAL;
+   
+ --L트림은 오른쪽에 지정된 문자열을 왼쪽 문자열에서 제거를 해라는 함수이다./ R은 오른쪽부터/ 그냥트림은 싹다제거
+ SELECT LTRIM('    smaple@example.com    ', '    ') AS Col1
+ 	  , RTRIM('    smaple@example.com    ', '    ') Col2
+ 	  , TRIM('    smaple@example.com    ') AS Col3
+ 	  , TRIM(LEADING '-' FROM '----saple@example.com----') AS Col4
+ 	  , TRIM(TRAILING '-' FROM '----saple@example.com----') AS Col5
+ 	  , TRIM(BOTH '-' FROM '----saple@example.com----') AS Col6
+ 	  FROM DUAL;
+ 	 
+ 	 --패드문은 여백을 만들고 싶을때 사용한다.
+ SELECT '!' || LPAD('A', 4) AS Col1
+      ,'!' || LPAD('AB', 4) AS Col2
+      ,'!' || LPAD('ABC', 4) AS Col3
+      , RPAD('A', 4) || '!' AS Col4
+      , RPAD('AB', 4) || '!' AS Col5
+      , RPAD('ABC', 4) || '!' AS Col6
+      , RPAD('A', 4, '0') || '!' AS Col7
+      , RPAD('ABCDE', 4) || '!' AS Col8 --초과되면 결과값이 잘림
+     FROM DUAL;
+    
+    --원하는 문자열을 잘라내는 함수     시작위치, 글자수
+ SELECT SUBSTR('smaple@example.com',1, 6) AS Col1
+ 	  , SUBSTR('smaple@example.com',8, 7) AS Col2
+ 	  , SUBSTR('smaple@example.com',-18, 6) AS Col3
+ 	  , SUBSTR('smaple@example.com',-11, 7) AS Col4
+ 	  , SUBSTR('smaple@example.com',-11) AS Col5 --문자열 길이를 따로 지정하지 않으면 지정한 시작위치부터 끝까지 잘라낸다.
+ 	 FROM DUAL;
+ 	
+ 	
+ SELECT LOWER('smaple@example.com') AS Col1 -- 다소문자
+      , UPPER('smaple@example.com') AS Col2 -- 다대문자
+      , INITCAP('smaple@example.com') AS Col3 -- 시작 이니셜만 대문자
+   FROM DUAL;
+  
+  --합치는거
+ SELECT  CONCAT('smaple', '@example.com') AS Col1
+   FROM DUAL;
+  
+  --             원본 문자열           ,  찾을 문자열     ,  변경 문자열
+ SELECT REPLACE('smaple@example.com', 'example.com', 'example.co.kr') AS Col1
+   FROM DUAL;
+  
+  --절대값
+ SELECT ABS(10) AS Col1
+      , ABS(-10) AS Col2
+   FROM DUAL;
+  
+  -- 나머지 계산
+ SELECT MOD(10, 3) AS Col1
+      , MOD(-10, 3) AS Col2
+ 	  , MOD(10.5, 3) AS Col3
+ 	  , MOD(-10.5, 3) AS Col4
+  FROM DUAL;
+  
+ --반올림
+ SELECT ROUND(10.4) AS Col1
+      , ROUND(10.5) AS Col2
+      , ROUND(10.45) AS Col3
+      , ROUND(10.45, 2) AS Col4
+      , ROUND(10.456, 2) AS Col5
+      , ROUND(18.5, -1) AS Col6 -- -1정수 첫째자리까지 표기해라
+  FROM DUAL;
+ 
+ SELECT FLOOR (10.34) AS Col1 -- 그냥 반내림
+ 	  , CEIL(10.34) AS Col2 -- 그냥 반올림
+ 	  , FLOOR (-10.34) AS Col3
+ 	  , CEIL(-10.34) AS Col4
+   FROM DUAL;
+  
+  -- 원하는 자리까지 남기고 잘라낸다.
+ SELECT TRUNC(10.34) AS Col1
+      , TRUNC(10.34, 1) AS Col2
+      , TRUNC(10.3456, 2) AS Col3
+      , TRUNC(1234, -1) AS Col4
+   FROM DUAL;   
+  
+ SELECT SYSDATE AS Col1
+ 	  , ADD_MONTHS(SYSDATE, 2) AS Col2 -- 현재 날짜에 2개월을 더해라
+ 	  , ADD_MONTHS(SYSDATE, -2) AS Col3 -- 현재 날짜에 2개월을 빼라
+ 	  , LAST_DAY(SYSDATE) AS Col4 -- 현재달의 마지막 날짜 출력 
+ 	  , NEXT_DAY(SYSDATE, 6) AS Col5 -- 현재날짜를 기준으로 1: 일요일 2: 월요일 ... 6: 금요일, 7 : 토요일
+ 	  , NEXT_DAY(SYSDATE, '금') AS Col6 
+	FROM DUAL; 
+
+ SELECT MONTHS_BETWEEN(SYSDATE, ADD_MONTHS(SYSDATE, 2)) AS Col1 -- 현재 날짜에서 오른쪽 날짜를 빼라
+      , MONTHS_BETWEEN(ADD_MONTHS(SYSDATE, 2), SYSDATE) AS Col2 -- 이 함수는 몇개월 차이인지만 보여줄 수 있다.
+   FROM DUAL;   
+   
+ SELECT EXTRACT(YEAR FROM SYSDATE) AS Col1 -- 지금 몇년도이냐
+      , EXTRACT(MONTH FROM SYSDATE) AS Col2 -- 지금 몇월이냐
+      , EXTRACT(DAY FROM SYSDATE) AS Col3 -- 지금 며칠이냐
+      , EXTRACT(HOUR FROM SYSTIMESTAMP) AS Col4 -- 지금 몇시냐
+      , EXTRACT(MINUTE FROM SYSTIMESTAMP) AS Col5 -- 지금 몇분이냐
+      , EXTRACT(SECOND FROM SYSTIMESTAMP) AS Col6 -- 지금 몇초냐
+   FROM DUAL;
+  
+  
+ SELECT SYSDATE + 1 AS Col1 -- 현재일자에서 더하기 빼기 해줌 --일자만 가능
+      , SYSDATE + 2 AS Col2
+      , SYSDATE - 2 AS Col3
+      , SYSDATE - 1 AS Col4
+      , SYSDATE + INTERVAL '1' DAY AS Col5 -- 하루 플러스 시켜라
+      , SYSDATE + INTERVAL '2' DAY AS Col6 -- 이틀 플러스 시켜라
+      , SYSDATE - INTERVAL '2' DAY AS Col7 -- 하루 마이너스 시켜라
+      , SYSDATE - INTERVAL '2' DAY AS Col8 -- 이틀 마이너스 시켜라
+      , SYSDATE + INTERVAL '1' MONTH AS  Col9 -- 이틀 마이너스 시켜라
+      , SYSDATE + INTERVAL '1' YEAR AS  Col10 -- 이틀 마이너스 시켜라
+      , SYSDATE + INTERVAL '1' HOUR AS  Col11 -- 이틀 마이너스 시켜라
+      , SYSDATE + INTERVAL '1' MINUTE AS  Col12 -- 이틀 마이너스 시켜라
+      , SYSDATE + INTERVAL '1' SECOND AS  Col13 -- 이틀 마이너스 시켜라
+   FROM DUAL;    
+  --형변환
+ SELECT 1234 AS Col1
+      , TO_CHAR(1234) AS Col2 
+      , TO_CHAR(1000000, '999,999,999')  
+      , TO_CHAR(1000000, 'L999,999,999') -- L 은 통화단위 표기  
+      , TO_CHAR(1000000, '000,000,000')  
+      , TO_CHAR(1000000, '000,000,000L')  --뒤에다 쓰면 뒤에 표기
+      , TO_CHAR(SYSDATE) AS Col3 -- 날짜를 문자열로 변환
+      , TO_CHAR(SYSDATE, 'YYYYMMDD') AS Col4 -- 날짜를 문자열로 변환
+      , TO_CHAR(SYSDATE, 'YYYY/MM/DD') AS Col5 -- 날짜를 문자열로 변환
+      , TO_CHAR(SYSDATE, 'YYYY-MM-DD') AS Col6 -- 날짜를 문자열로 변환
+      , TO_CHAR(SYSDATE, 'YYYY.MM.DD') AS Col7 -- 날짜를 문자열로 변환
+      , TO_CHAR(SYSDATE, 'YYYY"년" MM"월" DD"일"') AS Col7 -- 날짜를 문자열로 변환
+   FROM DUAL; 
+ 
+  -- 숫자를 날짤형식으로 변환
+ SELECT TO_DATE(20220425) AS Col1
+      , TO_DATE('20220425') AS CoL2
+      , TO_DATE('2022/04/25') AS CoL3
+      , TO_DATE('2022-04-25') AS CoL4
+      , TO_DATE('2022.04.25') AS CoL5
+      , TO_DATE('2022년 04월 25일', 'YYYY"년" MM"월" DD"일"') AS CoL6
+ FROM DUAL;
+
+	--넘버는 문자열만 입력값으로 받는다.
+SELECT TO_NUMBER('12345')
+     , TO_NUMBER('123.45')
+     , TO_NUMBER('123,456', '999,999')
+     , TO_NUMBER('FF', 'XX') -- 16진수
+  FROM DUAL;   
+ 
+ 
+ -- 날짜를 숫자로 바꾸고싶을 때 
+SELECT TO_NUMBER(TO_CHAR(SYSDATE, 'YYYYMMDD'))
+	FROM DUAL;
+   
+  
--- db_pro01/Scripts/Script.sql
+++ db_pro01/Scripts/Script.sql
@@ -1,11 +1,126 @@
+-- 직원 테이블
 SELECT * FROM EMPLOYEES;
 
+SELECT EMPLOYEE_ID
+	   ,FIRST_NAME
+	   ,LAST_NAME
+	FROM EMPLOYEES;
+
+SELECT EMPLOYEE_ID
+	  , FIRST_NAME
+	  , LAST_NAME
+ FROM EMPLOYEES
+WHERE EMPLOYEE_ID >= 200
+  AND FIRST_NAME = 'Pat';
+ 
+ 
+ SELECT EMPLOYEE_ID
+	  , FIRST_NAME
+	  , LAST_NAME
+ FROM EMPLOYEES
+WHERE EMPLOYEE_ID >= 200
+   OR FIRST_NAME = 'Steven'; -- 문자열 리터럴은 작은 따옴표
+  
+  
+ SELECT EMPLOYEE_ID AS "사번" --컬럼명으로 알 수 없을 때 내가 따로 별칭을 부여
+ 	 , FIRST_NAME AS 이름
+ 	 , LAST_NAME 성
+ 	 , EMAIL "이메일 주소" -- 별칭도 띄어쓰기가 들어가면 안된다. 띄어쓸거면 ""로 묶으면 된다.
+ 	FROM EMPLOYEES;
+ 
+ SELECT EMPLOYEE_ID AS "사번"--3 프롬절 웨얼절 셀렉트절 순으로 해석된다.
+ 	  , FIRST_NAME AS "이름"
+ 	  , LAST_NAME AS "성"
+   FROM EMPLOYEES --1
+  WHERE EMPLOYEE_ID >= 200; --2  
+  
+  
+  SELECT FIRST_NAME || ' ' || LAST_NAME AS "이름" -- ||는 문자열을 결합할 때 사용
+  		,SALARY * 12 AS "연봉"
+	FROM EMPLOYEES;  
+
+--  조건절에서 사용하는 연산자
+-- AND, OR, NOT, IN, NOT IN, BETWEEN ... AND 
+-- LIKE, NOT LIKE, IS NULL, IS NOT NULL 
+SELECT *
+  FROM EMPLOYEES 
+ WHERE NOT EMPLOYEE_ID >= 200;
+
+SELECT *
+  FROM EMPLOYEES 
+ WHERE EMPLOYEE_ID IN (200, 201, 202, 203); --소괄호 안에있는 내용만 반영해라
+ 
+ SELECT *
+  FROM EMPLOYEES 
+ WHERE EMPLOYEE_ID NOT IN (200, 201, 202, 203);
+
+SELECT *
+  FROM EMPLOYEES 
+ WHERE EMPLOYEE_ID BETWEEN 200 AND 203;
+
+SELECT *
+  FROM EMPLOYEES 
+ WHERE FIRST_NAME  LIKE 'S%';--S로 시작하는 이름을 불러옴
+ 
+ SELECT *
+  FROM EMPLOYEES 
+ WHERE FIRST_NAME  LIKE '%en';--en로 끝나는 이름을 불러옴
+ 
+ 
+ SELECT *
+  FROM EMPLOYEES 
+ WHERE FIRST_NAME  LIKE '%ea%';--en로 끝나는 이름을 불러옴
+ 
+ 
+ SELECT *
+  FROM EMPLOYEES 
+ WHERE FIRST_NAME  LIKE '_____';--_갯수에 맞춰 글자를 불러온다 _가 5개면 5글자 불러옴
+ 
+ 
+ SELECT *
+  FROM EMPLOYEES 
+ WHERE FIRST_NAME  LIKE 'D____';--D는 포함하는 5글자를 불러옴
+
+ 
+ 
+ SELECT *
+  FROM EMPLOYEES 
+ WHERE JOB_ID  LIKE '%#____' ESCAPE '#'; --앞의 글자는 뭐가와도 상관이없고 %_*** 이런 형식의 데이터를 불러오고 싶은것
+ 
+  SELECT *
+  FROM EMPLOYEES 
+ WHERE JOB_ID NOT LIKE '%#____' ESCAPE '#';
+
+SELECT *
+  FORM EMPLOYEES
+ WHERE COMMISSION_PCT IS NULL; --해당 테이블의 값이 NULL인 것을 불러와라
+
+SELECT *
+  FORM EMPLOYEES
+ WHERE COMMISSION_PCT IS NOT NULL;
+
+--()로 계산 순서 조정
+SELECT *
+  FROM EMPLOYEES
+ WHERE NOT EMPLOYEE_ID >= 200
+ 		OR EMPLOYEE_ID = 100;
+ 	
+ SELECT *
+  FROM EMPLOYEES
+ WHERE NOT (EMPLOYEE_ID >= 200
+ 		OR EMPLOYEE_ID = 100);	
+-- 부서 테이블
 SELECT * FROM DEPARTMENTS;
 
+-- 직급 테이블
 SELECT * FROM JOBS;
 
+-- 국가 테이블
 SELECT * FROM COUNTRIES;
 
+-- 국가별 지역 테이블
 SELECT * FROM LOCATIONS;
 
+-- 대륙 테이블
 SELECT * FROM REGIONS;
+
